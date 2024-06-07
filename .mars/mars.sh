c09b1a301fb57a6a62f92341329e687af7c487b9
Rafael Nascimento <rafael124715@gmail.com>
2024-06-07 09:53:17 -0300

    mars script

---
+++ .mars/mars.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+clear
+echo "#############################################################################################"
+echo "Initializing MIPS Assembler and runtime Simulator"
+echo "#############################################################################################"
+echo ""
+echo "(Ctrl+C to close the application)"
+java -jar /home/rafael/Assembly/.mars/Mars4_5.jar
+echo ""
