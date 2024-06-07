7ac8f94affaaf017d672930cad5a9b47b37d4dca
Rafael Nascimento <rafael124715@gmail.com>
2024-06-07 10:00:44 -0300

    Intallation Update

--- install.sh
+++ install.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+echo ""
 echo "##############################################################################################"
 echo "Criando scrip de execução MARS"
 echo "##############################################################################################"
 echo ""
+sleep 2
 
 pathAssembly=$(pwd)				# Caminho do Diretorio de Instalação
 
@@ -10,6 +12,13 @@ mkdir ~/bin					# Criando pasta bin para scripts
 cp $pathAssembly/.mars/mars.sh ~/bin/mars.sh	# Copiando Script para a pasta ~/bin
 chmod u+x ~/bin/mars.sh				# Habilitando modo executável
 
-echo "Para abrir execute no terminal: mars.sh"
+echo "Para abrir o programa, execute no terminal: mars.sh"
+
+sleep 2
+
+echo "Abrindo Mars em 3s ..."
+
+sleep 3
 
 mars.sh
+
