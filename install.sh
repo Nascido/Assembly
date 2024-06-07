31c3506b2824a5ecbb8e411a971ad6d64ac5bea5
Rafael Nascimento <rafael124715@gmail.com>
2024-06-07 11:02:00 -0300

    update install.sh

--- install.sh
+++ install.sh
@@ -21,32 +21,35 @@ sleep 2
 
 not_answer=true
 
-while [$not_answer]
+while $not_answer
 do
+  echo ""
   read -n1 -p "Deseja abrir o MARS agora? [ y , n ]" doit
-  
   case $doit in
-    'y'|'Y') 
-      open_mars=true 
-      not_answer=true
+    y|Y)
+	open_mars=true
+	not_answer=false
       ;;
-      
-    'n'|'N') 
-      open_mars=false
-      not_answer=true
-      ;;
-      
+
+    n|N)
+	open_mars=false
+	not_answer=false
+	;;
+
     *)
-      echo "y para SIM e n para NAO ..."
-      echo ""
-      ;;
+	echo ""
+	echo "APENAS Y para SIM e N para NAO ..."
+	echo ""
+        ;;
   esac
 done
 
-if [$open_mars]
-then 
+if $open_mars
+then
+  echo ""
   echo "Abrindo Mars em 3s ..."
   sleep 3
 
   mars.sh
 fi
+echo ""
