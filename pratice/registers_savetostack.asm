885e364e7122ab3d9748516b56d8c3b8cf8b67c5
Rafael Nascimento <rafael124715@gmail.com>
2024-06-20 09:21:10 -0300

    adding nested procedure

--- pratice/registers_savetostack.asm
+++ pratice/registers_savetostack.asm
@@ -6,34 +6,47 @@
 		
 		jal increaseRegister
 		
+		jal printNewline
+		jal printValue
+		
+		jal endProgram
+		
+
+	increaseRegister:
+		addi	$sp, $sp, -8
+		sw	$s0, 0($sp)
+		sw	$ra, 4($sp)
+
+		addi	$s0, $s0, 30
+		
+		# Nested Procedure
+		
+		jal printValue
+		
+		# Load the old value
+		lw $s0, 0($sp)
+		lw $ra, 4($sp)
+		
+		addi $sp, $sp, 8
+		
+		jr $ra
+	
+	printValue:
+		# Print Value
+		li $v0, 1
+		move $a0, $s0
+		syscall
+		
+		jr $ra
+
+	printNewline:
 		# Print a New Line
 		li $v0, 4
 		la $a0, newline
 		syscall
+		jr $ra
 		
-		# Print Value
-		li	$v0, 1
-		move	$a0, $s0
-		syscall
-		
+	endProgram:
 		# End Program
 		li	$v0, 10
 		syscall
-
-	increaseRegister:
-		addi	$sp, $sp, -4
-		sw	$s0, 0($sp)
-		
-		addi	$s0, $s0, 30
-		
-		# Print New Value in function
-		li	$v0, 1
-		move $a0, $s0
-		syscall
-		
-		# Load the old value
-		lw $s0, 0($sp)
-		addi $sp, $sp, 4
-		
-		jr $ra
-	
